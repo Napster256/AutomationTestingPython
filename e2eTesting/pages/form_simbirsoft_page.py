e50d666a31e444bccfaeab1be0c9e30937c5a09d
Albert Roldugin <a.roldugin@initlab.ru>
2026-04-07 17:25:46 +0500

    added a positive and negative case for the SimbirSoft Form

---
+++ e2eTesting/pages/form_simbirsoft_page.py
@@ -0,0 +1,105 @@
+import allure
+from pytest import fail
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoAlertPresentException
+from pages.base_page import BasePage
+
+class FormPage(BasePage):
+
+    URL = "https://practice-automation.com/form-fields/"
+
+    NAME = (By.ID, "name-input")
+    PASSWORD = (By.CSS_SELECTOR, "#feedbackForm input[type=password]")
+
+    DRINKS = {
+        "Milk": (By.XPATH, "//input[@id='drink2']"),
+        "Coffee": (By.XPATH, "//input[@id='drink3']")
+    }
+
+    COLORS = {
+        "Yellow": (By.XPATH, "//input[@id='color3']")
+    }
+
+    LIKE_AUTOMATION = (By.ID, "automation")
+
+    EMAIL = (By.ID, "email")
+    MESSAGE = (By.ID, "message")
+    SUBMIT = (By.ID, "submit-btn")
+
+    AUTOMATION_TOOLS = (By.XPATH, "//*[@id='feedbackForm']/ul/li")
+
+    @allure.step("Открыть страницу формы")
+    def open_page(self):
+        self.open(self.URL)
+        return self
+
+    @allure.step("Заполнить имя: {name}")
+    def fill_name(self, name):
+        self.fill(self.NAME, name)
+        return self
+
+    @allure.step("Заполнить пароль")
+    def fill_password(self, password):
+        self.fill(self.PASSWORD, password)
+        return self
+
+    @allure.step("Выбрать напитки: {drinks}")
+    def select_drinks(self, drinks: list):
+      for drink in drinks:
+        locator = self.DRINKS[drink]
+        element = self.browser.find_element(*locator)
+        self.browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+        self.click(locator)
+      return self
+
+    @allure.step("Выбрать цвет: {color}")
+    def select_color(self, color):
+        self.click(self.COLORS[color])
+        return self
+
+    @allure.step("Выбрать вариант автоматизации: {option}")
+    def select_automation(self, option="yes"):
+        dropdown = Select(self.browser.find_element(*self.LIKE_AUTOMATION))
+        dropdown.select_by_value(option.lower())
+        return self
+
+    @allure.step("Заполнить email: {email}")
+    def fill_email(self, email):
+        self.fill(self.EMAIL, email)
+        return self
+
+    @allure.step("Сформировать сообщение")
+    def fill_message_dynamic(self):
+        tools = self.browser.find_elements(*self.AUTOMATION_TOOLS)
+        tools_texts = [tool.text for tool in tools]
+
+        count = len(tools_texts)
+        longest_tool = max(tools_texts, key=len)
+
+        message = f"{count} tools. Longest: {longest_tool}"
+        self.fill(self.MESSAGE, message)
+        return self
+
+    @allure.step("Нажать Submit")
+    def submit(self):
+      element = self.browser.find_element(*self.SUBMIT)
+      self.browser.execute_script("arguments[0].click();", element)
+      return self
+
+
+    @allure.step("Проверить алерт")
+    def verify_alert(self,should_be_present=True):
+        try:
+            alert = self.browser.switch_to.alert
+            text = alert.text
+
+            allure.attach(text, name="Alert text", attachment_type=allure.attachment_type.TEXT)
+
+            assert text == "Message received!"
+            alert.accept()
+        except NoAlertPresentException:
+        # Если alert был нужен (True) — кидаем ошибку.
+        # Если alert отсутствует (False) — всё ок, тест прошел
+            if should_be_present:
+              fail("Форма не была отправлена, (alert не появился)")
---
+++ e2eTesting/tests/test_form_simbirsoft.py
@@ -0,0 +1,41 @@
+import allure
+import pytest
+from pages.form_simbirsoft_page import FormPage
+from utils.data_generator import generate_user
+
+
+@allure.feature("Form Tests")
+class TestForm:
+
+    @allure.title("Позитивный тест отправки формы")
+    def test_form_positive(self, browser):
+        user = generate_user()
+        page = FormPage(browser)
+
+        page.open_page() \
+            .fill_name(user["username"]) \
+            .fill_password(user["password"]) \
+            .select_drinks(["Milk", "Coffee"]) \
+            .select_color("Yellow") \
+            .select_automation("yes") \
+            .fill_email("name@example.com") \
+            .fill_message_dynamic() \
+            .submit() \
+            .verify_alert()
+
+    @allure.title("Негативный тест: пустой email")
+    def test_form_negative_empty_email(self, browser):
+        user = generate_user()
+        page = FormPage(browser)
+
+        page.open_page() \
+            .fill_name(user["username"]) \
+            .fill_password(user["password"]) \
+            .select_drinks(["Milk", "Coffee"]) \
+            .select_color("Yellow") \
+            .select_automation("no") \
+            .fill_email("") \
+            .fill_message_dynamic() \
+            .submit()
+        # Передаем False, так как в негативном тесте Alert быть не должно
+        page.verify_alert(should_be_present=False)
